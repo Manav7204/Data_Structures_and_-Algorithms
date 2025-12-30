b95a2a7a4e4bf4b4372a6cfa88588e0dfd551e03
Manav7204 <manavpatil47@gmail.com>
2025-12-30 14:45:26 +0530

    Dice problem

---
+++ Basic Problems GFG/closest_num.cpp
@@ -0,0 +1,37 @@
+//Dice Problem
+
+#include <iostream>
+using namespace std;
+
+int opposite_face(int n) {
+    int ans;
+    if (n == 1) {
+        ans = 6; 
+    }
+    else if (n == 2)
+    {
+        ans = 5;
+    }
+    else if (n == 3) {
+        ans = 4;
+    }
+    else if (n == 4) {
+        ans = 3;
+    }
+    else if (n == 5) {
+        ans = 2;
+    }
+    else if (n == 6) {
+        ans = 1;
+    }
+    
+    return ans;
+}
+
+int main () {
+    int face_num;
+    cin >> face_num;
+
+    cout << opposite_face(face_num) << endl;
+    return 0;
+}
