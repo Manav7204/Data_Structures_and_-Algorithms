f499eec8b16a7aaa40f2fc50cbaee9cde32da519
Manav7204 <manavpatil47@gmail.com>
2025-12-30 15:30:17 +0530

    .

--- Basic Problems GFG/arith_prog.cpp
+++ Basic Problems GFG/arith_prog.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 //Using 'For' loop
+// int nthTermOfAP(int a1,int a2, int n) {
+//     int nth_term = a1;
+//     int d = a2-a1;
+//     for (int i = 1; i <n; i++) {
+//         nth_term += d; 
+//     }
+//     return nth_term;
+// }
+
+//Formula based
 int nthTermOfAP(int a1,int a2, int n) {
-    int nth_term = a1;
-    int d = a2-a1;
-    for (int i = 1; i <n; i++) {
-        nth_term += d; 
-    }
+    int nth_term = a1 + (n -1) * (a2-a1);
     return nth_term;
 }
+
 int main () {
     int a1, a2, n;
     cin >> a1;
