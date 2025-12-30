0ca5f91874d65631670a19bd03ec44b5e4ce6ea6
Manav7204 <manavpatil47@gmail.com>
2025-12-30 15:14:56 +0530

    AP

---
+++ Basic Problems GFG/arith_prog.cpp
@@ -0,0 +1,21 @@
+//Nth Term of AP
+#include <iostream>
+using namespace std;
+
+//Using 'For' loop
+int nthTermOfAP(int a1,int a2, int n) {
+    int nth_term = a1;
+    int d = a2-a1;
+    for (int i = 1; i <n; i++) {
+        nth_term += d; 
+    }
+    return nth_term;
+}
+int main () {
+    int a1, a2, n;
+    cin >> a1;
+    cin >> a2;
+    cin >> n;
+    cout << nthTermOfAP(a1, a2, n);
+    return 0;
+}
--- Basic Problems GFG/closest_num.cpp
+++ Basic Problems GFG/closest_num.cpp
@@ -3,28 +3,35 @@
 #include <iostream>
 using namespace std;
 
-int opposite_face(int n) {
-    int ans;
-    if (n == 1) {
-        ans = 6; 
-    }
-    else if (n == 2)
-    {
-        ans = 5;
-    }
-    else if (n == 3) {
-        ans = 4;
-    }
-    else if (n == 4) {
-        ans = 3;
-    }
-    else if (n == 5) {
-        ans = 2;
-    }
-    else if (n == 6) {
-        ans = 1;
-    }
+//Naive approach
+// int opposite_face(int n) {
+//     int ans;
+//     if (n == 1) {
+//         ans = 6; 
+//     }
+//     else if (n == 2)
+//     {
+//         ans = 5;
+//     }
+//     else if (n == 3) {
+//         ans = 4;
+//     }
+//     else if (n == 4) {
+//         ans = 3;
+//     }
+//     else if (n == 5) {
+//         ans = 2;
+//     }
+//     else if (n == 6) {
+//         ans = 1;
+//     }
     
+//     return ans;
+// }
+
+//Using sum of two sides
+int opposite_face(int n) {
+    int ans = 7 - n;
     return ans;
 }
 
